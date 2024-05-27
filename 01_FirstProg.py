2ad48ca043d865f4fbd17a4d70f5de62eafddb3c
Ramakant Debata <ramakant.s.debata@gmail.com>
2024-05-27 15:47:50 +0530

    First Prog

---
+++ 01_FirstProg.py
@@ -0,0 +1,19 @@
+# import math.sqrt
+from math import sqrt as msqr
+
+def sqrt(num):
+  print("Not Implemented")
+
+# print(msqr(9))
+
+
+def Foo():
+  print("Hello")
+  print("World")
+
+# Foo()
+
+# print(type(Foo))
+
+
+import this
